6ae9f26b825f6fca100702cd38e2396aa5d977bf
IhorKoptilov <ihorkio@gmail.com>
2023-03-28 20:41:52 +0300

    Initial commit

---
+++ dz_2.py
@@ -0,0 +1,25 @@
+a=2
+b=5
+c=a==b
+print(c)
+PI=3.14
+print(b-a+PI)
+d=[a, b, c, PI]
+print(set(d))
+e=(1,3,5)
+f=("mon", "tue", 'wen')
+g=e+f
+print(list(g))
+dictionary = {
+    'mon' : 1,
+    'tue' : 'Tuesday',
+    3 : 'wed',
+    True : None,
+    False : [2, 4, 6],
+    3.46 : {
+        "3":'.46',
+        3 : .46
+    },
+    (5, 3, 1) : (6, 4, 2)
+}
+print(dictionary)
