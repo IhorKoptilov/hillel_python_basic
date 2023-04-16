31d32d7230adf0a54be52543ca89d5844160097a
IhorKoptilov <ihorkio@gmail.com>
2023-04-16 22:11:56 +0300

    Update dz_5.py

--- dz_5.py
+++ dz_5.py
@@ -1,2 +1,27 @@
 first = 'Python'
 print(first[::-1])
+len_first = len(first)
+list_first = list(first)
+three_first = str(list_first).split()
+print(list_first)
+print(three_first)
+
+
+def string_to_dictionary(text=str):
+    list1 = list(text)
+    count = text.count(text)
+    dict1 = dict.fromkeys(list1, count)
+    print(dict1)
+    return
+
+
+def string_to_dictionary1(text1=str):
+    list2 = list(text1)
+    count1 = list2.__len__()
+    dict2 = dict.fromkeys(text1, count1)
+    print(dict2)
+    return
+
+
+def max_string():
+    
