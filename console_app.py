2d5506807d5179f990e17c8a77e45faa198d8cbf
IhorKoptilov <ihorkio@gmail.com>
2023-05-25 15:07:22 +0300

    Update console_app.py

--- console_app.py
+++ console_app.py
@@ -28,7 +28,7 @@ class Pagination:
         if self.current_page > 0:
             self.current_page -= 1
         else:
-            print('No previous page')
+            raise ValueError('No previous page')
 
     # def get_current_page_data(self):
     #     start = (self.page_number - 1) * self.page_size
@@ -71,7 +71,9 @@ class App:
                 ' 1 : Add course''\n',
                 '2 : Show courses''\n',
                 '3 : Add student to the course''\n',
-                '4 : Exit',
+                '4 : Delete course''\n',
+                '5 : Delete student from the course''\n',
+                '6 : Exit',
             )
             item = input('Choose menu item: ')
             if item == '1':
@@ -81,6 +83,10 @@ class App:
             elif item == '3':
                 self.add_student()
             elif item == '4':
+                self.delete_course()
+            elif item == '5':
+                self.delete_student()
+            elif item == '6':
                 self.exit()
             else:
                 print('No such menu item. Please, try again')
@@ -99,36 +105,143 @@ class App:
         else:
             print('Select a course to add a student:')
             pagination = Pagination(courses)
-            self.pagination = iter(pagination)
-            current_page = 0
             while True:
-                start_index = current_page * pagination.items_per_page
-                for index, course in enumerate(next(self.pagination), start=start_index):
+                start_index = pagination.current_page * pagination.items_per_page
+                courses_on_page = next(pagination, [])
+                for index, course in enumerate(courses_on_page, start=start_index):
                     course_number = index + 1
-                    print(f'{course_number} - {course["name"]}')
-                command = input("Choose a course or see next/prev page: ")
+                    students = course['students']
+                    if students:
+                        student_names = [f"{student['first name']} {student['last name']}" for student in students]
+                        students_str = ", ".join(student_names)
+                        print(f'{course_number} - {course["name"]}: {students_str}')
+                    else:
+                        print(f'{course_number} - {course["name"]}: No students')
+                command = input("Choose a course or see next/prev page: ").strip()
                 if command == 'next':
                     try:
-                        self.pagination.next_page()
-                        current_page += 1
+                        pagination.next_page()
                     except ValueError as e:
                         print(e)
                 elif command == 'prev':
                     try:
-                        self.pagination.prev_page()
-                        current_page -= 1
+                        pagination.prev_page()
                     except ValueError as e:
                         print(e)
-                elif command.isdigit() and 1 <= int(command) <= len(self.pagination.data):
-                    selected_course = self.pagination.data[int(command) - 1]
-                    first_name = input("Enter student's first name: ")
-                    last_name = input("Enter student's last name: ")
-                    selected_course['students'].append({'first name': first_name, 'last name': last_name})
+                elif command.isdigit() and 1 <= int(command) <= len(courses_on_page) + start_index:
+                    selected_course = courses[int(command) - 1]
+                    students = selected_course['students']
+                    first_name = input('Enter student\'s first name: ').strip()
+                    last_name = input('Enter student\'s last name: ').strip()
+                    new_student = {'first name': first_name, 'last name': last_name}
+                    students.append(new_student)
                     self.storage.write_to_file()
-                    print("Student added successfully!")
+                    print('Student added successfully!')
                     break
                 else:
                     print('Invalid command')
+
+    def delete_course(self):
+        courses = self.storage.data.get('courses', [])
+        if not courses:
+            print('No added courses')
+        else:
+            print('Select a course to delete:')
+            pagination = Pagination(courses)
+            while True:
+                start_index = pagination.current_page * pagination.items_per_page
+                courses_on_page = next(pagination, [])
+                for index, course in enumerate(courses_on_page, start=start_index):
+                    course_number = index + 1
+                    students = course['students']
+                    if students:
+                        student_names = [f"{student['first name']} {student['last name']}" for student in students]
+                        students_str = ", ".join(student_names)
+                        print(f'{course_number}: {course["name"]}, Students: {students_str}')
+                    else:
+                        print(f'{course_number}: {course["name"]}, No students')
+                command = input("Choose a course or see next/prev page: ").strip()
+                if command == 'next':
+                    try:
+                        pagination.next_page()
+                    except ValueError as e:
+                        print(e)
+                elif command == 'prev':
+                    try:
+                        pagination.prev_page()
+                    except ValueError as e:
+                        print(e)
+                elif command.isdigit() and 1 <= int(command) <= len(courses_on_page) + start_index:
+                    course_index = int(command) - 1
+                    selected_course = courses[course_index]
+                    del courses[course_index]
+                    print(f'Course "{selected_course["name"]}" deleted successfully')
+                    self.storage.write_to_file()
+                    break
+                else:
+                    print('Invalid input. Please try again')
+
+    def delete_student(self):
+        courses = self.storage.data.get('courses', [])
+        if not courses:
+            print('No added courses')
+        else:
+            print('Select a course to delete a student from:')
+            pagination = Pagination(courses)
+            while True:
+                start_index = pagination.current_page * pagination.items_per_page
+                courses_on_page = next(pagination, [])
+                for index, course in enumerate(courses_on_page, start=start_index):
+                    course_number = index + 1
+                    students = course['students']
+                    if students:
+                        student_names = [f"{student['first name']} {student['last name']}" for student in students]
+                        students_str = ", ".join(student_names)
+                        print(f'{course_number}: {course["name"]}, Students: {students_str}')
+                    else:
+                        print(f'{course_number}: {course["name"]}, No students')
+                command = input("Choose a course or see next/prev page: ").strip()
+                if command == 'next':
+                    try:
+                        pagination.next_page()
+                    except ValueError as e:
+                        print(e)
+                elif command == 'prev':
+                    try:
+                        pagination.prev_page()
+                    except ValueError as e:
+                        print(e)
+                elif command.isdigit() and 1 <= int(command) <= len(courses_on_page) + start_index:
+                    course_index = int(command) - 1
+                    selected_course = courses[course_index]
+                    students = selected_course['students']
+                    if not students:
+                        print('No students in this course')
+                        break
+                    print('Select a student to delete:')
+                    while True:
+                        for index, student in enumerate(students, start=1):
+                            student_number = index
+                            print(f'{student_number}: {student["first name"]} {student["last name"]}')
+                        try:
+                            option = input('Enter student number (0 to cancel): ')
+                            if option == '0':
+                                break
+                            option = int(option)
+                            student_index = option - 1
+                            del students[student_index]
+                            print('Student deleted successfully')
+                            self.storage.write_to_file()
+                            break
+                        except ValueError:
+                            print('Invalid input. Please try again')
+                        except IndexError:
+                            print('Invalid student number. Please try again')
+                        except Exception as e:
+                            print(f'An error occurred: {str(e)}')
+                    break
+                else:
+                    print('Invalid input. Please try again')
 
     def show_courses(self):
         courses = self.storage.data.get('courses', [])
@@ -137,7 +250,14 @@ class App:
         else:
             print('Courses:')
             for index, course in enumerate(courses, start=1):
-                print(f'{index} - {course["name"]}')
+                students = course['students']
+                if students:
+                    students_str = ', '.join(
+                        [f"{i + 1}. {student['first name']} {student['last name']}" for i, student in
+                         enumerate(students)])
+                    print(f'{index} - {course["name"]} (Students: {students_str})')
+                else:
+                    print(f'{index} - {course["name"]} (No students in this course)')
 
     # def select_courses(self):
     #     courses = self.storage.data.get('courses', [])
