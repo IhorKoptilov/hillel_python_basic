491a5d6e0c12c994de483552dc6135f83ceec90e
IhorKoptilov <ihorkio@gmail.com>
2023-05-25 23:52:33 +0300

    Update console_app.py

--- console_app.py
+++ console_app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import math
 
 
 class Pagination:
@@ -19,7 +20,7 @@ class Pagination:
         return items
 
     def next_page(self):
-        if self.current_page < self.get_total_pages() - 1:
+        if self.current_page < self.get_total_pages():
             self.current_page += 1
         else:
             raise ValueError('No next page')
@@ -37,7 +38,8 @@ class Pagination:
     #     return self.current_page_data
 
     def get_total_pages(self):
-        return len(self.data) // self.items_per_page + (len(self.data) % self.items_per_page > 0)
+        # return len(self.data) // self.items_per_page + (len(self.data) % self.items_per_page > 0)
+        return math.ceil(len(self.data) / self.items_per_page)
 
 
 class FileStorage:
@@ -117,7 +119,7 @@ class App:
                         print(f'{course_number} - {course["name"]}: {students_str}')
                     else:
                         print(f'{course_number} - {course["name"]}: No students')
-                command = input("Choose a course or see next/prev page: ").strip()
+                command = input("Choose a course number or see next/prev page: ").strip()
                 if command == 'next':
                     try:
                         pagination.next_page()
@@ -160,7 +162,7 @@ class App:
                         print(f'{course_number}: {course["name"]}, Students: {students_str}')
                     else:
                         print(f'{course_number}: {course["name"]}, No students')
-                command = input("Choose a course or see next/prev page: ").strip()
+                command = input("Choose a course name or see next/prev page: ").strip()
                 if command == 'next':
                     try:
                         pagination.next_page()
@@ -171,12 +173,31 @@ class App:
                         pagination.prev_page()
                     except ValueError as e:
                         print(e)
-                elif command.isdigit() and 1 <= int(command) <= len(courses_on_page) + start_index:
-                    course_index = int(command) - 1
-                    selected_course = courses[course_index]
-                    del courses[course_index]
-                    print(f'Course "{selected_course["name"]}" deleted successfully')
+                elif command.isalpha():
+                    matching_courses = [course for course in courses if course['name'].lower() == command.lower()]
+                    if len(matching_courses) == 1:
+                        selected_course = matching_courses[0]
+                    elif len(matching_courses) > 1:
+                        print(f"There are {len(matching_courses)} courses with the name '{command}'.")
+                        print("Please select the course by its number:")
+                        for index, course in enumerate(matching_courses, start=1):
+                            print(f"{index}: {course['name']}")
+                        try:
+                            selected_number = int(input("Enter the number of the course to delete: "))
+                            if 1 <= selected_number <= len(matching_courses):
+                                selected_course = matching_courses[selected_number - 1]
+                            else:
+                                print('Invalid input. Please try again')
+                                continue
+                        except ValueError:
+                            print('Invalid input. Please try again')
+                            continue
+                    else:
+                        print('No course found with that name')
+                        continue
+                    courses.remove(selected_course)
                     self.storage.write_to_file()
+                    print(f'Course "{selected_course["name"]}" deleted successfully')
                     break
                 else:
                     print('Invalid input. Please try again')
@@ -193,14 +214,14 @@ class App:
                 courses_on_page = next(pagination, [])
                 for index, course in enumerate(courses_on_page, start=start_index):
                     course_number = index + 1
-                    students = course['students']
+                    students = course.get('students', [])
                     if students:
                         student_names = [f"{student['first name']} {student['last name']}" for student in students]
                         students_str = ", ".join(student_names)
                         print(f'{course_number}: {course["name"]}, Students: {students_str}')
                     else:
                         print(f'{course_number}: {course["name"]}, No students')
-                command = input("Choose a course or see next/prev page: ").strip()
+                command = input("Choose a course name or see next/prev page: ").strip()
                 if command == 'next':
                     try:
                         pagination.next_page()
@@ -211,35 +232,68 @@ class App:
                         pagination.prev_page()
                     except ValueError as e:
                         print(e)
-                elif command.isdigit() and 1 <= int(command) <= len(courses_on_page) + start_index:
-                    course_index = int(command) - 1
-                    selected_course = courses[course_index]
-                    students = selected_course['students']
+                elif command.isalpha():
+                    matching_courses = [course for course in courses if course['name'].lower() == command.lower()]
+                    if len(matching_courses) == 1:
+                        selected_course = matching_courses[0]
+                    elif len(matching_courses) > 1:
+                        print(f"There are {len(matching_courses)} courses with the name '{command}'.")
+                        print("Please select the course by its number:")
+                        for index, course in enumerate(matching_courses, start=1):
+                            print(f"{index}: {course['name']}")
+                        try:
+                            selected_number = int(input("Enter the number of the course to delete a student from: "))
+                            if 1 <= selected_number <= len(matching_courses):
+                                selected_course = matching_courses[selected_number - 1]
+                            else:
+                                print('Invalid input. Please try again')
+                                continue
+                        except ValueError:
+                            print('Invalid input. Please try again')
+                            continue
+                    else:
+                        print('No course found with that name')
+                        continue
+
+                    students = selected_course.get('students', [])
                     if not students:
                         print('No students in this course')
                         break
-                    print('Select a student to delete:')
-                    while True:
-                        for index, student in enumerate(students, start=1):
-                            student_number = index
-                            print(f'{student_number}: {student["first name"]} {student["last name"]}')
+                    print(f'Students in course "{selected_course["name"]}":')
+                    for index, student in enumerate(students, start=1):
+                        print(f'{index}: {student["first name"]} {student["last name"]}')
+
+                    first_name = input("Enter the first name of the student to delete: ")
+                    matching_students = [student for student in students if
+                                         student['first name'].lower() == first_name.lower()]
+                    if len(matching_students) == 1:
+                        selected_student = matching_students[0]
+                        students.remove(selected_student)
+                        print('Student deleted successfully')
+                        self.storage.write_to_file()
+                        break
+                    elif len(matching_students) > 1:
+                        print(f"There are {len(matching_students)} students with the first name '{first_name}'.")
+                        print("Please select the student by their number:")
+                        for index, student in enumerate(matching_students, start=1):
+                            print(f"{index}: {student['first name']} {student['last name']}")
                         try:
-                            option = input('Enter student number (0 to cancel): ')
-                            if option == '0':
+                            selected_number = int(input("Enter the number of the student to delete: "))
+                            if 1 <= selected_number <= len(matching_students):
+                                selected_student = matching_students[selected_number - 1]
+                                students.remove(selected_student)
+                                print('Student deleted successfully')
+                                self.storage.write_to_file()
                                 break
-                            option = int(option)
-                            student_index = option - 1
-                            del students[student_index]
-                            print('Student deleted successfully')
-                            self.storage.write_to_file()
-                            break
+                            else:
+                                print('Invalid input. Please try again')
+                                continue
                         except ValueError:
                             print('Invalid input. Please try again')
-                        except IndexError:
-                            print('Invalid student number. Please try again')
-                        except Exception as e:
-                            print(f'An error occurred: {str(e)}')
-                    break
+                            continue
+                    else:
+                        print('No student found with that first name')
+                        continue
                 else:
                     print('Invalid input. Please try again')
 
@@ -274,6 +328,6 @@ class App:
 
 
 if __name__ == '__main__':
-    file_path = input('Enter storage path: ')
+    file_path = "test1.json"
     app = App(FileStorage.load_from_file(file_path))
     app.run()
